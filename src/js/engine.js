782d1de9c283b8a5504859dae844be987769421f
Ryotruuser <ryonantruuser@gmail.com>
2025-07-28 17:20:05 -0300

    nave agora consegue se movimentar em 2d e não vaza da tela

--- src/js/engine.js
+++ src/js/engine.js
@@ -1,5 +1,37 @@
 const state = {
+    view: {
+        gameView: document.querySelector(".main__content2-game"),
+    },
     sprites: {
         ship: document.querySelector(".game__ship"),
+        shipPosition: 140,
     }
 }
+
+function moveShip(){
+    window.addEventListener("keydown", (e) => {
+        if(state.view.gameView.style.display === "flex"){
+            if(e.key==="ArrowLeft"){
+                state.sprites.shipPosition -= 10;
+                if(state.sprites.shipPosition <= -10){
+                    state.sprites.shipPosition = -10;
+                }
+            }else if(e.key==="ArrowRight"){
+                state.sprites.shipPosition += 10;
+                if(state.sprites.shipPosition >= 290){
+                    state.sprites.shipPosition = 290;
+                }      
+            }
+        }
+        state.sprites.ship.style.left = state.sprites.shipPosition + "px";
+        console.log(state.sprites.shipPosition)
+        
+    })
+}
+
+
+function init () {
+    moveShip();
+}
+
+init();
