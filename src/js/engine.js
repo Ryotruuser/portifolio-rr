379fb28a84b1183b48add8b8ee7f95d241cddf9e
Ryotruuser <ryonantruuser@gmail.com>
2025-07-28 18:52:21 -0300

    joguinho escondido rodando ok, ja e possivel movimentar 2d e adicionar e remover as balas da nave

--- src/js/engine.js
+++ src/js/engine.js
@@ -5,7 +5,16 @@ const state = {
     sprites: {
         ship: document.querySelector(".game__ship"),
         shipPosition: 140,
+        cannon: document.querySelector(".game__ship-canon"),
+        cannonPosition: 170,
+        shootPosition: 0,
     }
+}
+
+function cannonShot(){
+    let shoot = document.createElement("li");
+    shoot.classList.add("game__ship-shoot");
+    state.sprites.cannon.appendChild(shoot);
 }
 
 function moveShip(){
@@ -13,25 +22,34 @@ function moveShip(){
         if(state.view.gameView.style.display === "flex"){
             if(e.key==="ArrowLeft"){
                 state.sprites.shipPosition -= 10;
+                state.sprites.cannonPosition -= 10;
+                setInterval(cannonShot(), 20);
                 if(state.sprites.shipPosition <= -10){
                     state.sprites.shipPosition = -10;
+                    state.sprites.cannonPosition = 20;
+                    state.sprites.cannon.innerHTML = "";
                 }
             }else if(e.key==="ArrowRight"){
                 state.sprites.shipPosition += 10;
+                state.sprites.cannonPosition +=10;
+                setInterval(cannonShot(), 20);
                 if(state.sprites.shipPosition >= 290){
                     state.sprites.shipPosition = 290;
+                    state.sprites.cannonPosition = 320;
+                    state.sprites.cannon.innerHTML = "";
                 }      
             }
         }
-        state.sprites.ship.style.left = state.sprites.shipPosition + "px";
-        console.log(state.sprites.shipPosition)
         
+        state.sprites.ship.style.left = state.sprites.shipPosition + "px";
+        state.sprites.cannon.style.left = state.sprites.cannonPosition + "px"; 
     })
 }
 
 
 function init () {
     moveShip();
+    
 }
 
 init();
